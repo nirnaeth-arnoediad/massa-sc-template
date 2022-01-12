f56dd4d86353778f3fee08fe213b00e688dfdda6
Adrien Zinger <zinger.ad@gmail.com>
2022-01-12 17:32:48 +0100

    Update binary and example

--- src/createSC.ts
+++ src/createSC.ts
@@ -3,10 +3,14 @@
  * smartcontract.
  **/
 
-import { create_sc, include_base64, print } from "./massa";
+import { create_sc, include_base64, print, call, get_call_stack } from "./massa";
+import { JSON } from 'json-as';
 
 export function main(_args: string): i32 {
     const bytes = include_base64('./build/helloworld.wasm');
-    print(`Smart contract created at address ${create_sc(bytes)}`);
+    let addr = create_sc(bytes);
+    print("Address = " + addr);
+    print(call(addr, "helloworld", "Adrien!"));
+    print(`${JSON.parse<string[]>(get_call_stack())[0]}`)
     return 0;
 }
