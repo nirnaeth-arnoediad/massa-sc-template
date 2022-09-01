3b812ccbb3b42ef2e97860d19b2f1a9af39ea8cc
Nirnaeth <57898835+nirnaeth-arnoediad@users.noreply.github.com>
2022-09-01 16:59:42 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@
  * which is ready to be send on Massa network node!
  **/
 
-import { create_sc, include_base64, generate_event, call, Context } from "massa-sc-std";
+import { createSC, fileToBase64, generateEvent, call, Context } from "massa-sc-std";
 import { JSON } from 'json-as';
 
 export function main(_args: string): void {
-    const bytes = include_base64('./build/smart-contract.wasm');
-    let addr = create_sc(bytes);
-    generate_event("Address = " + addr);
+    const bytes = fileToBase64('./build/smart-contract.wasm');
+    let addr = createSC(bytes);
+    generateEvent("Address = " + addr);
     // Here example of how to call a smart contract
-    generate_event(call(addr, "helloworld", "World", 0));
-    generate_event(`${Context.get_call_stack()[0]}`)
+    generateEvent(call(addr, "helloworld", "World", 0));
+    generateEvent(`${Context.caller}`);
 }
